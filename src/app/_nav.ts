4b22b7510968e2cb1912b7b183ee568fef53a035
Osmany Torres Leyva <osmany.torres@datys.cu>
2018-02-25 07:26:25 -0500

    Ajusta menú por defecto

--- src/app/_nav.ts
+++ src/app/_nav.ts
@@ -3,14 +3,14 @@ export const navigation = [
     name: 'Dashboard',
     url: '/dashboard',
     icon: 'icon-speedometer',
-    badge: {
-      variant: 'info',
-      text: 'NEW'
-    }
   },
   {
     name: 'CRUD',
     url: '/crud',
     icon: 'icon-user',
+    badge: {
+      variant: 'info',
+      text: 'NEW'
+    }
   },
 ];
